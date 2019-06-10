d5bf7b862271f0713dc3a18c225e42c671bf27ec
Alistair Francis <alistairfrancis@msslsu.mssl.ucl.ac.uk>
2019-06-10 17:20:02 +0100

    add clustering analysis

--- src/agglomerative_analysis.py
+++ src/agglomerative_analysis.py
@@ -7,6 +7,7 @@ import cluster_surveys
 import distance
 import iou
 
+
 def load_all_surveys(xml_master_dir):
     assert os.path.isdir(xml_master_dir), 'xml_dir is not a directory!'
     dfs = []
@@ -24,22 +25,41 @@ def calc_all_nodetrees(dfs,distance_metric):
 
 if __name__=='__main__':
     import sys
+    import matplotlib.pyplot as plt
+
 
     xml_master_dir = sys.argv[1]
     dfs = load_all_surveys(xml_master_dir)
-    nodes = calc_all_nodetrees(dfs,distance.negative_jaccard_with_diameter_priority)
+    nodes = calc_all_nodetrees(dfs,distance.negative_jaccard)
 
-    dists = [0.4+0.1*i for i in range(6)]
+    dists = [0.7+0.01*i for i in range(30)]
     clusters = []
     n_clusters = np.empty([len(dists),len(nodes)])
+    n_singles = np.empty([len(dists),len(nodes)])
+    n_multi = np.empty([len(dists),len(nodes)])
+
     for i,d in enumerate(dists):
         clusters_at_d = []
         for j,tile in enumerate(nodes):
             clusters_at_d.append(cluster_surveys.clusters_at_distance(tile,d))
             n_clusters[i,j] = len(clusters_at_d[j])
-            print(n_clusters)
+            n_singles[i,j] = len([c for c in clusters_at_d[j] if len(c)==1])
+            n_multi[i,j] = len([c for c in clusters_at_d[j] if len(c)>1])
+
         clusters.append(clusters_at_d)
 
+    # print(np.array(n_clusters))
 
-    print(clusters)
-    print(n_clusters)
+    fig,ax = plt.subplots()
+    hists = []
+    for d,cluster_set in zip(dists,clusters):
+        hists.append(np.zeros(6))
+        for tile in cluster_set:
+            cluster_sizes = [len(cluster) for cluster in tile]
+            this_hist,_ = np.histogram(cluster_sizes,bins=[0.5,1.5,2.5,3.5,4.5,5.5,6.5])
+            # print(this_hist)
+            hists[-1] = hists[-1] + this_hist
+            # print(hists[-1])
+        ax.semilogy([1,2,3,4,5,6],hists[-1])
+        print(hists[-1])
+    plt.show()
--- src/cluster_slider.py
+++ src/cluster_slider.py
@@ -8,13 +8,14 @@ import cluster_surveys
 from data import convert
 import distance
 
+
 def get_update_func(nodelist,surveys):
     cmp = matplotlib.cm.ScalarMappable(norm=None, cmap='cool').get_cmap()
     def update(val):
         dist = sdist.val
         clusters = cluster_surveys.clusters_at_distance(nodelist,dist)
         colour_intensity = np.array([len(cluster) for cluster in clusters])
-        cols = cmp(colour_intensity/6)
+        cols = cmp(colour_intensity/N_surveyors)
         cols[:,-1] = 0.05+colour_intensity/15
         centres = convert.clusters2np(clusters,surveys)
         cluster_ec = EllipseCollection(widths=centres[:,2],heights=centres[:,2],angles=0,units='xy',
@@ -23,7 +24,10 @@ def get_update_func(nodelist,surveys):
         del ax_main.collections[0]
         ax_main.add_collection(cluster_ec)
         ax_hist.clear()
-        ax_hist.hist([len(cluster) for cluster in clusters],[0.5+i for i in range(6)],rwidth=0.4)
+        n,bins,patches = ax_hist.hist([len(cluster) for cluster in clusters],[0.5+i for i in range(N_surveyors+1)],rwidth=0.4)
+        for i,patch in enumerate(patches):
+            patch.set_facecolor(color=cmp(i/N_surveyors))
+        print(len(clusters),'    ',end='\r')
         ax_hist.grid(True)
         fig.canvas.draw_idle()
 
@@ -45,6 +49,7 @@ if __name__=='__main__':
     img = imread(img_path)
     xml_dir = sys.argv[2]
     surveys = [convert.xml2df(os.path.join(xml_dir,s_i)) for s_i in os.listdir(xml_dir)]
+    N_surveyors = len(surveys)
     nodelist = cluster_surveys.agglomerative(surveys,distance.negative_jaccard)
     fig= plt.figure()
     ax_main = plt.axes([0.2,0.12,0.75,0.75])
@@ -54,7 +59,7 @@ if __name__=='__main__':
     clusters = cluster_surveys.clusters_at_distance(nodelist,dist)
     centres = convert.clusters2np(clusters,surveys)
     colour_intensity = np.array([len(cluster) for cluster in clusters])
-    cols = cmp(colour_intensity/6)
+    cols = cmp(colour_intensity/N_surveyors)
     cols[:,-1] = 0.05+colour_intensity/15
 
     cluster_ec = EllipseCollection(widths=centres[:,2],heights=centres[:,2],angles=0,units='xy',
@@ -64,7 +69,9 @@ if __name__=='__main__':
     axcolor = 'lightgoldenrodyellow'
 
     ax_hist = plt.axes([0.05,0.05,0.18,0.6])
-    ax_hist.hist([len(cluster) for cluster in clusters],[0.5+i for i in range(6)],rwidth=0.4)
+    n,bins,patches = ax_hist.hist([len(cluster) for cluster in clusters],[0.5+i for i in range(N_surveyors+1)],rwidth=0.4)
+    for i,patch in enumerate(patches):
+        patch.set_facecolor(color=cmp(i/N_surveyors))
     ax_hist.grid(True)
 
     ax_dist = plt.axes([0.35, 0.05, 0.5, 0.02], facecolor=axcolor)
